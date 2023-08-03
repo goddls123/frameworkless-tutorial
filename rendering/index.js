318b72ecd051b6ef2f1c247ca00916af4a0fb3ae
Jun <coolch0916@gmail.com>
2023-08-03 09:12:30 +0900

    add register file

--- rendering/index.js
+++ rendering/index.js
@@ -2,16 +2,21 @@ import getTodos from "./getTodos.js";
 import counterView from './view/counter.js'
 import filtersView from './view/filters.js'
 import todosView from './view/todos.js'
-
+import registry from "./registry.js";
 
 const state ={
     todos :getTodos(),
     currentFilter : 'All'
 }
 
-const main = document.querySelector('.todoapp')
+registry.add('filters',filtersView)
+registry.add('counter',counterView)
+registry.add('todos',todosView)
+
+
 
 window.requestAnimationFrame(()=>{
-    const newMain = view(main, state)
+    const main = document.querySelector('.todoapp')
+    const newMain = registry.renderRoot(main, state)
     main.replaceWith(newMain)
 })
---
+++ rendering/registry.js
@@ -0,0 +1,41 @@
+
+const registry = {}
+
+const add =(name, component)=>{
+    registry[name] =renderWrapper(component);
+}
+
+const renderRoot = (root,state)=>{
+    const cloneComponent = root=>{
+        return root.cloneNode(true);
+    }
+
+    return renderWrapper(cloneComponent)(root,state)
+}
+
+
+const renderWrapper = component=>{
+    return (targetElement,state)=>{
+        const element = component(targetElement,state)
+
+        const childComponents =element.querySelectorAll(['data-component'])
+
+        const child = Array.from(childComponents)
+                            .forEach((target)=>{
+                                const name = target.dataset.component
+                                
+                                const child = registry[name]
+
+                                if(!child){
+                                    return
+                                }
+
+                                target.replaceWith(child(target,state))
+                            })
+        
+        return element
+    }
+}
+
+
+export default {add, renderRoot}
