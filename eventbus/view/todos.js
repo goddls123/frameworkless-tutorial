e4981a03a71844404c955b915053a0c2a505aacc
Jun <coolch0916@gmail.com>
2023-11-16 17:55:31 +0900

    change

--- eventbus/view/todos.js
+++ eventbus/view/todos.js
@@ -1,108 +1,101 @@
-import eventCreators from "../model/eventCreators.js"
+import eventCreators from "../model/eventCreators.js";
 
-let template
+let template;
 
+const getNewTodoNode = () => {
+  if (!template) {
+    template = document.getElementById("todo-item");
+  }
 
-const getNewTodoNode = () => { 
-    if (!template) {
-        template = document.getElementById('todo-item')
+  return template.content.firstElementChild.cloneNode(true);
+};
+
+const getTodoElement = (todo, index, dispatch) => {
+  const { text, completed } = todo;
+
+  const element = getNewTodoNode();
+
+  element.querySelector("input.edit").value = text;
+  element.querySelector("label").textContent = text;
+
+  if (completed) {
+    element.classList.add("completed");
+    element.querySelector("input.toggle").checked = true;
+  } else {
+    if (element.classList.contains("completed")) {
+      element.classList.remove("completed");
+    }
+    element.querySelector("input.toggle").checked = false;
+  }
+
+  element.querySelector("button.destroy").dataset.index = index;
+  element.querySelector("input.toggle").dataset.index = index;
+
+  attachEventsToTodoElement(element, index, dispatch);
+
+  return element;
+};
+
+const attachEventsToTodoElement = (element, index, dispatch) => {
+  element.addEventListener("dblclick", () => {
+    element.classList.add("editing");
+    element.querySelector("input.edit").focus();
+  });
+
+  element.querySelector("input.edit").addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+      element.classList.remove("editing");
+      dispatch(eventCreators.updateItem(index, e.target.value));
     }
 
-    return template.content.firstElementChild.cloneNode(true)
-}
-
-const getTodoElement = (todo, index, dispatch) =>{
-    const {text, completed} = todo
-
-    const element =getNewTodoNode()
-    
-
-    element.querySelector('input.edit').value = text;
-    element.querySelector('label').textContent = text; 
-
-    if (completed){
-        element.classList.add('completed')
-        element.querySelector('input.toggle').checked=true;
+    if (e.key === "Escape") {
+      element.classList.remove("editing");
     }
-    else {
-        if (element.classList.contains('completed')){
-            element.classList.remove('completed')
-        }
-        element.querySelector('input.toggle').checked=false;
+  });
+};
+const filterTodos = (todos, currentFilter) => {
+  const todoList = todos.map((todo, index) => {
+    return { ...todo, index };
+  });
+  const isCompleted = (todo) => todo.completed;
+  if (currentFilter === "Active") {
+    return todoList.filter((t) => !isCompleted(t));
+  }
+
+  if (currentFilter === "Completed") {
+    return todoList.filter((t) => isCompleted(t));
+  }
+
+  return [...todoList];
+};
+
+const attachEvents = (newTodoList, dispatch) => {
+  newTodoList.addEventListener("click", (e) => {
+    if (e.target.matches("button.destroy")) {
+      dispatch(eventCreators.deleteItem(parseInt(e.target.dataset.index)));
     }
-
-    element.querySelector('button.destroy').dataset.index =index
-    element.querySelector('input.toggle').dataset.index =index
-
-    attachEventsToTodoElement(element,index,dispatch)
-
-    return element
-}
-
-
-const attachEventsToTodoElement = (element,index,dispatch)=>{
-    element
-    .addEventListener('dblclick', () => {
-        element.classList.add('editing')
-        element
-        .querySelector('input.edit').focus()
-    })
-
-    element
-    .querySelector('input.edit')
-    .addEventListener('keydown', e => {
-        if (e.key === 'Enter') {
-        element.classList.remove('editing')
-        dispatch(eventCreators.updateItem(index, e.target.value))
-        }
-
-        if (e.key === 'Escape') {
-            element.classList.remove('editing')
-        }
-    })
-}
-const filterTodos = (todos,currentFilter)=>{
-
-    const todoList = todos.map((todo,index) => {return {...todo ,index}})
-    const isCompleted = todo => todo.completed
-    if (currentFilter === 'Active'){
-        return todoList.filter(t => !isCompleted(t))
+    if (e.target.matches("input.toggle")) {
+      dispatch(eventCreators.toggleComplete(parseInt(e.target.dataset.index)));
     }
+  });
+};
 
-    if (currentFilter === 'Completed'){
-        return todoList.filter(t => isCompleted(t))
-    }
+export default (targetElement, state, dispatch) => {
+  const { todos, currentFilter } = state;
 
-    return [...todoList]
-}
+  const newTodoList = targetElement.cloneNode(true);
 
-const attachEvents = (newTodoList,dispatch)=>{
+  newTodoList.innerHtml = "";
 
-    newTodoList.addEventListener('click', (e)=>{
-        if (e.target.matches('button.destroy')){
-            dispatch(eventCreators.deleteItem(parseInt(e.target.dataset.index)))
-        }
-        if (e.target.matches('input.toggle')){
-            dispatch(eventCreators.toggleComplete(parseInt(e.target.dataset.index)))
-        }
-    })
-}
+  const filterdTodos = filterTodos(todos, currentFilter);
 
-export default (targetElement, state , dispatch) =>{
+  filterdTodos
+    .map((todo) => getTodoElement(todo, todo.index, dispatch))
+    .forEach((element) => {
+      newTodoList.appendChild(element);
+    });
 
-    const {todos,currentFilter} = state
+  attachEvents(newTodoList, dispatch);
 
-    const newTodoList = targetElement.cloneNode(true);
-
-    newTodoList.innerHtml = ''
-
-    const filterdTodos = filterTodos(todos, currentFilter)
-
-    filterdTodos
-        .map((todo)=>getTodoElement(todo,todo.index, dispatch))
-        .forEach(element=>{newTodoList.appendChild(element)})
-    
-    attachEvents(newTodoList,dispatch)
-
-    return newTodoList
-}
+  return newTodoList;
+};
