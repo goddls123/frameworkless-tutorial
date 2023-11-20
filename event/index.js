5412047c51a9f05e23e770b77956bbd294f0531a
Jun <coolch0916@gmail.com>
2023-11-20 17:10:28 +0900

    eslint

--- event/index.js
+++ event/index.js
@@ -1,56 +1,52 @@
 import appView from "./view/app.js";
-import counterView from './view/counter.js'
-import filtersView from './view/filters.js'
-import todosView from './view/todos.js'
+import counterView from "./view/counter.js";
+import filtersView from "./view/filters.js";
+import todosView from "./view/todos.js";
 import registry from "./registry.js";
 import applyDiff from "./applyDiff.js";
 
-const state ={
-    todos :[],
-    currentFilter : 'All'
-}
+const state = {
+  todos: [],
+  currentFilter: "All",
+};
 
-
-registry.add('app',appView)
-registry.add('filters',filtersView)
-registry.add('counter',counterView)
-registry.add('todos',todosView)
+registry.add("app", appView);
+registry.add("filters", filtersView);
+registry.add("counter", counterView);
+registry.add("todos", todosView);
 
 const events = {
-    addItem: (text)=>{
-        state.todos.push({
-            text,
-            completed:false
-        })
-        render()
-    },
-    deleteItem: (index)=>{
-        state.todos.splice(index,1)
-        render()
-    },
-    toggleCompleted: (index)=>{
-        state.todos[index].completed = !state.todos[index].completed
-        render()
-    },
-    clearCompleted:()=>{
-        state.todos = state.todos.filter(todo => !todo.completed)    
-        render();
-    },
-    changeFilter:(filter)=>{
-        state.currentFilter=filter
-        render()
-    }
-    
-}
+  addItem: (text) => {
+    state.todos.push({
+      text,
+      completed: false,
+    });
+    render();
+  },
+  deleteItem: (index) => {
+    state.todos.splice(index, 1);
+    render();
+  },
+  toggleCompleted: (index) => {
+    state.todos[index].completed = !state.todos[index].completed;
+    render();
+  },
+  clearCompleted: () => {
+    state.todos = state.todos.filter((todo) => !todo.completed);
+    render();
+  },
+  changeFilter: (filter) => {
+    state.currentFilter = filter;
+    render();
+  },
+};
 
+const render = () => {
+  window.requestAnimationFrame(() => {
+    const main = document.querySelector("#root");
+    const newMain = registry.renderRoot(main, state, events);
+    applyDiff(document.body, main, newMain);
+  });
+};
 
-const render =()=>{
-    window.requestAnimationFrame(()=>{
-        const main = document.querySelector('#root')
-        const newMain = registry.renderRoot(main, state, events)
-        applyDiff(document.body,main,newMain)
-    })
-}
-
-
-render()
+render();
